158055f3ffcba9dd1106bb56b7dbb4f11a813998
mdasif8393 <md.asif.8393@gmail.com>
2025-07-12 18:16:42 +0600

    27-7 Verify Token and protect route using middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import httpStatus from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 
 import validateRequest from "../../middlewares/validateRequest";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -12,6 +16,32 @@ router.post(
   UserControllers.createUser
 );
 
-router.get("/all-users", UserControllers.getAllUsers);
+router.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+
+      if (!accessToken) {
+        throw new AppError(httpStatus.NOT_FOUND, "No token receive");
+      }
+
+      const verifyToken = jwt.verify(accessToken as string, "secret");
+
+      console.log(verifyToken);
+      if ((verifyToken as JwtPayload).role !== Role.ADMIN) {
+        throw new AppError(
+          httpStatus.NOT_ACCEPTABLE,
+          "Your are not permitted to access this route"
+        );
+      }
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  UserControllers.getAllUsers
+);
 
 export const UserRoutes = router;
