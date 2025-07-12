4420939311d16ce796b46a1701cec9cc1da96242
mdasif8393 <md.asif.8393@gmail.com>
2025-07-12 09:31:47 +0600

    27-4 Fix sensitive password bugs, Password Hashing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,24 +2,31 @@ import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcrypt from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
 
   const isUserExists = await User.findOne({ email });
 
   // if user exists in database then show error
   if (isUserExists) {
-    throw new AppError(httpStatus.BAD_REQUEST, "User alreary exists");
+    throw new AppError(httpStatus.BAD_REQUEST, "User already exists");
   }
 
+  const hashedPassword = await bcrypt.hash(password as string, 10);
   // push authProvider inside user data
   const authProvider: IAuthProvider = {
     provider: "credentials",
     providerId: email as string,
   };
 
-  const user = await User.create({ email, auths: [authProvider], ...rest });
+  const user = await User.create({
+    email,
+    auths: [authProvider],
+    password: hashedPassword,
+    ...rest,
+  });
 
   return user;
 };
