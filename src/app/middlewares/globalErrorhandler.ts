35777a100165080e01cd2c533df4ec82fe2e633d
mdasif8393 <md.asif.8393@gmail.com>
2025-07-17 23:39:20 +0600

    29-8 Refactoring Error Handlers to separate functions

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,6 +2,10 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { handleZodError } from "../helpers/handleZodError";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const globalErrorhandler = (
@@ -12,21 +16,43 @@ export const globalErrorhandler = (
 ) => {
   let statusCode = 500;
   let message = `Something went wrong!! ${err.message}`;
+  let errorSources: any = [];
 
   // Mongoose Duplicate key error means if unique email duplicate
   if (err.code === 11000) {
-    const matchedArray = err.message.match(/"([^"]*)"/);
-    statusCode = 400;
-    message = `${matchedArray[1]} already exists`;
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
   }
   // Mongoose Cast error or Object Id error
   else if (err.name === "CastError") {
-    statusCode = 400;
-    message = "Invalid MongoDB ObjectId. Please provide a valid _id";
-  } else if (err instanceof AppError) {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+  // Mongoose Validation Error: like in boolean field send string
+  else if (err.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources;
+    message = simplifiedError.message;
+  }
+  // Zod Validation Error
+  else if (err.name === "ZodError") {
+    const simplifiedError = handleZodError(err);
+
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources;
+    message = simplifiedError.message;
+  }
+  // Custom AppError Class
+  else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
-  } else if (err instanceof Error) {
+  }
+  // Default Error Class
+  else if (err instanceof Error) {
     statusCode = 500;
     message = err.message;
   }
@@ -34,6 +60,7 @@ export const globalErrorhandler = (
   res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     err,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
